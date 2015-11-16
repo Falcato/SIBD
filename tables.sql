3cd8e0e7adbf8d904750e913b6e8950461298ef9
Tomás Falcato Costa <tomas.falcato.costa@ist.utl.pt>
2015-11-16 22:09:32 +0000

    tables.sql final v1.1

--- tables.sql
+++ tables.sql
@@ -12,38 +12,38 @@ drop table if exists pan;
 drop table if exists patient;
 
 create table patient
-   (patient_number numeric(20,2),
+   (patient_number numeric(20),
     patient_name varchar(255),
     patient_address varchar(255),
     primary key(patient_number));
 
 create table pan
    (pan_domain varchar(255),
-    pan_phone numeric(20,2),
+    pan_phone numeric(20),
     primary key(pan_domain));
 
 create table device
-   (device_serialnum numeric(20,2),
+   (device_serialnum numeric(20),
     device_manufacturer varchar(255),
     description varchar(255),
     primary key(device_serialnum, device_manufacturer));
 
 create table sensor
-   (sensor_snum numeric(20,2),
+   (sensor_snum numeric(20),
     sensor_manuf varchar(255),
     sensor_units varchar(255),
     primary key(sensor_snum, sensor_manuf),
     foreign key(sensor_snum, sensor_manuf) references device(device_serialnum, device_manufacturer));
 
 create table actuator
-   (actuator_snum numeric(20,2),
+   (actuator_snum numeric(20),
     actuator_manuf varchar(255),
     actuator_units varchar(255),
     primary key(actuator_snum, actuator_manuf),
     foreign key(actuator_snum, actuator_manuf) references device(device_serialnum, device_manufacturer));
 
 create table municipality
-   (municipality_nut4code numeric(20,2),
+   (municipality_nut4code numeric(20),
     municipality_name varchar(255),
     primary key(municipality_nut4code));
 
@@ -53,7 +53,7 @@ create table period
     primary key(period_start, period_end));
 
 create table reading
-   (reading_snum numeric(20,2),
+   (reading_snum numeric(20),
     reading_manuf varchar(255),
     reading_datetime timestamp,
     reading_value numeric(20,2),
@@ -61,7 +61,7 @@ create table reading
     foreign key(reading_snum, reading_manuf) references sensor(sensor_snum, sensor_manuf));
 
 create table setting
-   (setting_snum numeric(20,2),
+   (setting_snum numeric(20),
     setting_manuf varchar(255),
     setting_datetime timestamp,
     setting_value numeric(20,2),
@@ -71,7 +71,7 @@ create table setting
 create table wears
    (wears_start date,
     wears_end date,
-    wears_patient numeric(20,2),
+    wears_patient numeric(20),
     wears_pan varchar(255),
     primary key(wears_start, wears_end, wears_patient, wears_pan),
     foreign key(wears_start, wears_end) references period(period_start, period_end),
@@ -81,8 +81,8 @@ create table wears
 create table lives
    (lives_start date,
     lives_end date,
-    lives_patient numeric(20,2),
-    lives_muni numeric(20,2),
+    lives_patient numeric(20),
+    lives_muni numeric(20),
     primary key(lives_start, lives_end, lives_patient, lives_muni),
     foreign key(lives_start, lives_end) references period(period_start, period_end),
     foreign key(lives_patient) references patient(patient_number),
@@ -91,10 +91,11 @@ create table lives
 create table connects
    (connects_start date,
     connects_end date,
-    connects_snum numeric(20,2),
+    connects_snum numeric(20),
     connects_manuf varchar(255),
     connects_pan varchar(255),
     primary key(connects_start, connects_end, connects_snum, connects_manuf, connects_pan),
     foreign key(connects_start, connects_end) references period(period_start, period_end),
     foreign key(connects_snum, connects_manuf) references device(device_serialnum, device_manufacturer),
     foreign key(connects_pan) references pan(pan_domain));
+
