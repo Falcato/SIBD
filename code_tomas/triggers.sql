cc8f43dee100a633b76ba34ba2207c1ce052299f
falcato <tomas.falcato.costa@ist.utl.pt>
2015-11-18 23:50:40 +0000

    triggers v3

--- code_tomas/triggers.sql
+++ code_tomas/triggers.sql
@@ -4,9 +4,9 @@ create trigger check_overlapping_periods_wears_insert before insert on wears
 for each row
 begin
 
-	if new.wears_pan in (select wears_pan from wears where new.wears_pan = wears_pan)
-	 or new.wears_end < new.wears_start then	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
-	 and new.wears_pan in (select wears_pan from wears where new.wears_pan = wears_pan)
+	if (new.wears_start < some (select wears_end from wears where new.wears_pan = wears_pan)
+	 or new.wears_end < new.wears_start)	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
+	 and new.wears_pan in (select wears_pan from wears where new.wears_pan = wears_pan) then
 
 		call pan_already_in_use_1();
 		
@@ -18,8 +18,8 @@ create trigger check_overlapping_periods_wears_update before update on wears
 for each row
 begin
 
-	if new.wears_start < some (select wears_end from wears where wears_pan = new.wears_pan) /*PERGUNTAR AO PROF SE TEMOS DE NOS PREOCUPAR COM A DATA DE INICIO*/
-	 or new.wears_end < some (select wears_start from wears where wears_pan = new.wears_pan)  
+	if (new.wears_start < some (select wears_end from wears where wears_pan = new.wears_pan) /*PERGUNTAR AO PROF SE TEMOS DE NOS PREOCUPAR COM A DATA DE INICIO*/
+	 or new.wears_end < some (select wears_start from wears where wears_pan = new.wears_pan))
 	 and new.wears_patient != all (select wears_patient from wears where new.wears_pan = wears_pan) then
 		
 		call pan_already_in_use_2();
@@ -32,9 +32,9 @@ create trigger check_overlapping_periods_connects_insert before insert on connec
 for each row
 begin
 
-	if new.connects_pan in (select connects_pan from connects where new.connects_pan = connects_pan)
-	 or new.connects_end < new.connects_start then	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
-	 and new.connects_pan in (select connects_pan from connects where new.connects_pan = connects_pan)
+	if (new.connects_pan in (select connects_pan from connects where new.connects_pan = connects_pan)
+	 or new.connects_end < new.connects_start) 	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
+	 and new.connects_pan in (select connects_pan from connects where new.connects_pan = connects_pan) then
 		
 		call pan_already_in_use_3();
 		
