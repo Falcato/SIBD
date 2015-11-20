09e1972373e66a0506d5bb34840b7c4c1490704a
falcato <tomas.falcato.costa@ist.utl.pt>
2015-11-20 14:32:20 +0000

    triggers finais n vou olhar mais pa esta merda se quiserem vejam voces

--- code_tomas/testes_triggers.sql
+++ code_tomas/testes_triggers.sql
@@ -2,20 +2,29 @@ insert into patient values (1, 'ricky', 'fonte da vaca');
 insert into patient values (2, 'ze', 'almada');
 insert into patient values (3, 'tomas', 'alameda');
 insert into patient values (4, 'nabais', 'telheiras');
-insert into period values ('2015-11-10', '2999-11-10');
+
+insert into device values (11, 'philips', 'aaaa');
+insert into device values (11, 'nokia', 'aaaa');
+
 insert into period values ('2015-11-11', '2999-11-11');
-insert into period values ('2015-11-10', '2015-11-09');
-insert into period values ('2015-11-17', '2999-11-17');
+insert into period values ('2015-11-12', '2999-11-12');
 insert into period values ('2015-11-11', '2015-11-17');
+insert into period values ('2015-11-12', '2015-11-17');
 insert into period values ('2015-11-18', '2999-11-18');
+insert into period values ('2015-11-19', '2999-11-19');
 
 insert into pan values ('pan1', 999);
 insert into pan values ('pan2', 999);
 insert into pan values ('pan3', 999);
 
-insert into wears values ('2015-11-10', '2999-11-10', 1, 'pan1');	
-insert into wears values ('2015-11-11', '2999-11-11', 2, 'pan2');
-update wears set wears_start = '2015-11-10', wears_end = '2015-11-09' where wears_patient = 1 and wears_pan = 'pan1';
-insert into wears values ('2015-11-17', '2999-11-17', 1, 'pan3');
-update wears set wears_start = '2015-11-11', wears_end = '2015-11-17' where wears_patient = 2 and wears_pan = 'pan2';
-insert into wears values ('2015-11-18', '2999-11-18', 2, 'pan1');
+insert into wears values ('2015-11-11', '2999-11-11', 1, 'pan1');	
+insert into wears values ('2015-11-12', '2999-11-12', 2, 'pan2');
+update wears set wears_end = '2015-11-17' where wears_patient = 1 and wears_pan = 'pan1' and wears_start = '2015-11-11';
+/*insert into wears values ('2015-11-18', '2999-11-18', 1, 'pan2');*/
+
+insert into connects values ('2015-11-11', '2999-11-11', 11, 'philips', 'pan1');	
+insert into connects values ('2015-11-12', '2999-11-12', 11, 'nokia', 'pan2');
+update connects set connects_end = '2015-11-17' where connects_snum = 11 and connects_manuf = 'philips' and connects_pan = 'pan1' and connects_start = '2015-11-11';
+update connects set connects_end = '2015-11-17' where connects_snum = 11 and connects_manuf = 'nokia' and connects_pan = 'pan2' and connects_start = '2015-11-12';
+insert into connects values ('2015-11-18', '2999-11-18', 11, 'philips', 'pan2');
+insert into connects values ('2015-11-19', '2999-11-19', 11, 'nokia', 'pan2');
--- code_tomas/triggers.sql
+++ code_tomas/triggers.sql
@@ -4,8 +4,7 @@ create trigger check_overlapping_periods_wears_insert before insert on wears
 for each row
 begin
 
-	if (new.wears_start < some (select wears_end from wears where new.wears_pan = wears_pan)
-	 or new.wears_end < new.wears_start)	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
+	if new.wears_start < some (select wears_end from wears where new.wears_pan = wears_pan)	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
 	 and new.wears_pan in (select wears_pan from wears where new.wears_pan = wears_pan) then
 
 		call pan_already_in_use_1();
@@ -18,10 +17,8 @@ create trigger check_overlapping_periods_wears_update before update on wears
 for each row
 begin
 
-	if (new.wears_start < some (select wears_end from wears where wears_pan = new.wears_pan) /*PERGUNTAR AO PROF SE TEMOS DE NOS PREOCUPAR COM A DATA DE INICIO*/
-	 or new.wears_end < some (select wears_start from wears where wears_pan = new.wears_pan))
-	 and new.wears_patient != all (select wears_patient from wears where new.wears_pan = wears_pan)
-	 /*or new.wears_end < new.wears_start*/ then
+	if new.wears_end < some (select wears_start from wears where wears_pan = new.wears_pan)
+	 and new.wears_patient != all (select wears_patient from wears where new.wears_pan = wears_pan) then
 		
 		call pan_already_in_use_2();
 		
@@ -33,8 +30,7 @@ create trigger check_overlapping_periods_connects_insert before insert on connec
 for each row
 begin
 
-	if (new.connects_pan in (select connects_pan from connects where new.connects_pan = connects_pan)
-	 or new.connects_end < new.connects_start) 	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
+	if new.connects_start < some (select connects_end from connects where new.connects_pan = connects_pan) 	/*PERGUNTAR AO PROF SE PRECISAMOS DE VER OS TEMPOS*/
 	 and new.connects_pan in (select connects_pan from connects where new.connects_pan = connects_pan) then
 		
 		call pan_already_in_use_3();
@@ -47,11 +43,9 @@ create trigger check_overlapping_periods_connects_update before update on connec
 for each row
 begin
 
-	if new.connects_start < some (select connects_end from connects
-	 where connects_pan = new.connects_pan) and new.connects_end < some (select connects_start from connects
-	 where connects_pan = new.connects_pan) and new.connects_snum != all (select connects_snum from connects 
-     where new.connects_pan = connects_pan) or new.connects_manuf != all (select connects_manuf from connects 
-     where new.connects_pan = connects_pan) then
+	if new.connects_end < some (select connects_start from connects where connects_pan = new.connects_pan)
+	 and (new.connects_snum != all (select connects_snum from connects where new.connects_pan = connects_pan)
+	 or new.connects_manuf != all (select connects_manuf from connects where new.connects_pan = connects_pan)) then
 		
 		call pan_already_in_use_4();
 		
